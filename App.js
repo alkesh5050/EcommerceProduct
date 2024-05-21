da4eba5f3d23586e06d988d65e4645f149980293
alkesh5050 <alkeshbaldha0@gmail.com>
2024-05-21 17:32:52 +0530

    Ecommerce

---
+++ App.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text style={{fontFamily:'Metropolis-Bold',fontSize:90}}>App</Text>
+    </View>
+  )
+}
