9f12b507464f41dd76e3c49b8ed60370863877b0
alkesh5050 <alkeshbaldha0@gmail.com>
2024-06-08 18:46:22 +0530

    8_6_24_oll

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import Forgot from './App/Container/Forgot/Forgot';
 import HomePage from './App/Container/HomePage/HomePage';
 import Womens_top from './App/Container/Womens_tops/Womens_top';
 import Favorites from './App/Container/Favorites/Favorites';
+import OrderDetails from './App/Container/OrderDetails/OrderDetails';
+import ShippingAddresses from './App/Container/ShippingAddresses/ShippingAddresses';
 
 
 export default function App() {
@@ -14,13 +16,15 @@ export default function App() {
     <>
       {/* <Text style={{fontFamily:'Metropolis-Light',fontSize:90}}>App</Text>
       <FontAwesome name="facebook-square" size={50} color="red"/> */}
-   
-    {/* <Signup/> */}
-    {/* <Login/> */}
-    {/* <Forgot/> */}
-    {/* <HomePage/> */}
-    {/* <Womens_top/> */}
-    <Favorites/>
+
+      {/* <Signup/> */}
+      {/* <Login/> */}
+      {/* <Forgot/> */}
+      {/* <HomePage/> */}
+      {/* <Womens_top/> */}
+      {/* <Favorites/> */}
+      {/* <OrderDetails /> */}
+      <ShippingAddresses/>
     </>
   )
 }
